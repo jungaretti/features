81ee0f9a38a258318fd09f93f22b666f5df767db
JP Ungaretti <git@jungaretti.com>
2022-11-10 21:40:27 +0000

    Add ripgrep feature

---
+++ test/ripgrep/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "version" rg --version
